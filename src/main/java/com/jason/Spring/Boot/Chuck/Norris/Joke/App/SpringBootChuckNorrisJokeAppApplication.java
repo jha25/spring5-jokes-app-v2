8382b26a4b84fa650b512992c9dfd35f6cf9f080
Ha <630019@bah.com>
2023-05-16 15:25:58 -0500

    add new dependency

---
+++ src/main/java/com/jason/Spring/Boot/Chuck/Norris/Joke/App/SpringBootChuckNorrisJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.jason.Spring.Boot.Chuck.Norris.Joke.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChuckNorrisJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChuckNorrisJokeAppApplication.class, args);
+	}
+
+}
